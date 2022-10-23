4c430974f2cebd8091af947e0f8752ded4bafe55
Caius Durling <dev@caius.name>
2022-10-23 18:46:24 +0100

    Lean on ApiFinder in Tsclient.default_client

--- lib/tsclient.rb
+++ lib/tsclient.rb
@@ -5,25 +5,14 @@ loader = Zeitwerk::Loader.for_gem
 loader.setup
 
 module Tsclient
-  def self.default_client
+  def self.default_client(api_finder: FindApiUri.new)
     return @default_client if defined?(@default_client)
 
-    # Attempt to setup the default client from various known strategies
-    # Envariables override everything
-    uri = if ENV.key?("TS_LOCALAPI_PORT")
-      URI("http://:#{ENV["TS_LOCALAPI_KEY"]}@localhost:#{ENV["TS_LOCALAPI_PORT"]}")
-
-    # Running on macOS, need to find api deets from filesystem
-    elsif RUBY_PLATFORM["darwin"]
-      tsfile = Pathname.glob("#{ENV["HOME"]}/Library/Group Containers/*.io.tailscale.ipn.macos/sameuserproof-*-*").first
-      _, port, password = tsfile.basename.to_s.split("-", 3)
-      URI("http://:#{password}@localhost:#{port}")
-    # Throw our hands in the air, we just don't ~~care~~ know
+    if (uri = api_finder.call)
+      @default_client = Client.new(uri: uri)
     else
-      raise NotImplemented, "Can't figure out where localapi is"
+      raise Error.new("Could not find localapi on this machine")
     end
-
-    @default_client = Client.new(uri: uri)
   end
 end
 
